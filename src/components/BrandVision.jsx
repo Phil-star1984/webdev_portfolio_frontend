172378c09b21280a22fc88cd9534f75825476470
Philipp Mulfinger <info@phil-splash.de>
2025-03-29 16:20:08 +0100

    fix: padding text

--- src/components/BrandVision.jsx
+++ src/components/BrandVision.jsx
@@ -4,12 +4,13 @@ import { Container } from "react-bootstrap";
 function BrandVision() {
   return (
     <div className="brandvision_wrapper bg-dark">
-      <Container className="container_brandvision">
+      <Container className="px-3 container_brandvision">
         <h1 className="fs-2 fw-normal lh-sm text-secondary text-center">
           Philipp Mulfinger verbindet Funktionalität mit emotionalem Design. Als
-          Frontend Web Developer entwickelt er einzigartige gamification Lösungen für anspruchsvolle
-          Marken. Immer mit Fokus auf das Ziel nachhaltige und positive Veränderungen zu
-          schaffen für eine lebenswertere Zukunft.
+          Frontend Web Developer entwickelt er einzigartige gamification
+          Lösungen für anspruchsvolle Marken. Immer mit Fokus auf das Ziel
+          nachhaltige und positive Veränderungen zu schaffen für eine
+          lebenswertere Zukunft.
         </h1>
       </Container>
     </div>
